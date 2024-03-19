d27babf631d6637aa80900608bc146aa10a9022c
Alexandr-V1993 <sinyukovalexandr@gmail.com>
2024-03-20 00:35:09 +0300

    prob

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,6 @@ function Footer() {
   useEffect(() => {
     // Проверяем, что код выполняется только в продакшн
     if (process.env.NODE_ENV === "production") {
-      // Yandex.Metrika counter
       (function (m, e, t, r, i, k, a) {
         m[i] =
           m[i] ||
@@ -36,6 +35,14 @@ function Footer() {
         trackLinks: true,
         accurateTrackBounce: true,
       });
+
+      // Добавляем код для продакшн
+      (function (d, z, s) {
+        s.src = "https://" + d + "/401/" + z;
+        try {
+          (document.body || document.documentElement).appendChild(s);
+        } catch (e) {}
+      })("aistekso.net", 7243075, document.createElement("script"));
     }
   }, []);
 
@@ -57,12 +64,6 @@ function Footer() {
           © 2024
         </div>
       </div>
-      <script
-        src="https://alwingulla.com/88/tag.min.js"
-        data-zone="52463"
-        async
-        data-cfasync="false"
-      ></script>
     </footer>
   );
 }
